7c33dd0045a658766bce36272dd5a5616a22928b
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2023-09-07 00:11:42 +0700

    update test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ng-storefront-ui-playground' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-storefront-ui-playground');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
